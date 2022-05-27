1cb99153aef84672dc239ea372e8280fe5309746
HyeonsikBae <baehyeonsik@gmail.com>
2022-05-28 05:58:57 +0900

    feat: add friend list

--- src/component/nav/navFriendZone.tsx
+++ src/component/nav/navFriendZone.tsx
@@ -4,16 +4,70 @@ import * as theme from "../../theme/theme";
 import { ComponentNavFriendBox } from "./navFriendBox";
 
 const NavFriendZone = styled("div", {
+  margin: "5px",
   height: `${theme.NAV_FRIEND_HEIGHT}`,
+  overflowY: "scroll",
+  "&::-webkit-scrollbar": {
+    width: "5px",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: theme.NEON_RED,
+    borderRadius: "10px",
+  },
+  "&::-webkit-scrollbar-track": {
+    backgroundColor: "grey",
+    borderRadius: "10px",
+  },
 });
+
+type User = {
+  name: string;
+  profile_link: string;
+  status: string;
+}
+
+const friends: User[] = [
+  {
+    name: "skim1",
+    profile_link: "/asset/profileImage/skim.png",
+    status: "online",
+  },
+  {
+    name: "skim2",
+    profile_link: "/asset/profileImage/skim.png",
+    status: "offline",
+  },
+  {
+    name: "skim3",
+    profile_link: "/asset/profileImage/skim.png",
+    status: "in game",
+  },
+  {
+    name: "skim4",
+    profile_link: "/asset/profileImage/skim.png",
+    status: "??",
+  },
+  {
+    name: "skim5",
+    profile_link: "/asset/profileImage/skim.png",
+    status: "online",
+  }
+];
+
+const renderFrineds = () => {
+  const friendsList: any = [];
+  for (let i = 0; i < friends.length; i += 1) {
+    friendsList.push(
+      <ComponentNavFriendBox key={i} friend={friends[i]} />
+    );
+  }
+  return friendsList;
+};
 
 export function ComponentNavFriendZone() {
   return (
     <NavFriendZone>
-      <ComponentNavFriendBox name="Hello" />
-      <ComponentNavFriendBox name="spark" />
-      <ComponentNavFriendBox name="Hybae" />
-      <ComponentNavFriendBox name="kkim" />
+      {renderFrineds()}
     </NavFriendZone>
   );
 }
