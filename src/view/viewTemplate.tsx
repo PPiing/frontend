a714eb613071a0f376eaf52050f2e94bb945ad1a
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-06-08 16:28:20 +0900

    Update viewTemplate.tsx
    콘텐츠, 네비 사이 구분선 제거
    전체 뷰는 vw, vh로 크기 설정
    Left Box, Right Box는 %로 크기 설정 및 overflow 제거

--- src/view/viewTemplate.tsx
+++ src/view/viewTemplate.tsx
@@ -6,28 +6,24 @@ import { ContainerNavMenu } from "../container/navMenu";
 import { ContainerNavCommunity } from "../container/navCommunity";
 
 export const ContainerLeftBox = styled("div", {
-  width: "calc(80vw - 40px)",
-  height: "calc(100vh - 40px)",
+  width: "calc(80% - 40px)",
+  height: "calc(100% - 40px)",
   padding: "20px",
-  overflaw: "hidden",
   backgroundSize: "cover",
 });
 
 export const ContainerRightBox = styled("div", {
-  width: "calc(20vw - 40px)",
-  minWidth: "300px",
-  height: "calc(100vh - 40px)",
+  width: "calc(20% - 40px)",
+  height: "cale(100% - 40px)",
   padding: "20px",
-  overflaw: "hidden",
-  //   borderLeft: "3px solid white",
   boxShadow: "0 0 0.8rem #fff, 0 0 1.1rem #ffffff50,",
 });
 
 export const ViewWrapper = styled("div", {
   display: "flex",
   flexDirection: "row",
-  width: "100%",
-  height: "100%",
+  width: "100vw",
+  height: "100vh",
   overflow: "hidden",
 });
 
