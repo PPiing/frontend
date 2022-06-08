3dae6dab139aa22ae784a207a208610cc94e73ad
hyunja27 <123gk@naver.com>
2022-06-08 16:56:23 +0900

    :building_construction: 06/08_reconstructing_3DGame_Border_Frame #44

--- src/container/contentGame.tsx
+++ src/container/contentGame.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { styled, } from "@stitches/react";
 
 import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import { Backdrop, CameraShake, Center, Cloud, Lightformer, OrbitControls, Sky, SpotLight, Stage, Stars } from "@react-three/drei";
+import { Canvas, extend } from "@react-three/fiber";
+import { CameraShake, OrbitControls, Stars, Text3D, Text } from "@react-three/drei";
 import * as theme from "../theme/theme";
 import * as template from "./contentTemplate";
 
-const Contents = styled(template.Contents, {
-
-});
+// extend({ TextGeometry });
 
 // => 기본 game Stage 렌더링중 _ 주석을 지우지 마세요!
 
@@ -28,16 +26,83 @@ const Contents = styled(template.Contents, {
 // <CameraShake {...config} />
 // <Cloud opacity={0.5} speed={0.4} width={10} depth={1.5} segments={20} />
 
+// function Font() {
+//   return (
+//     <Text color="black" anchorX="center" anchorY="middle">
+//       hello world!
+//     </Text>
+//   );
+// }
+
+// function Basic2() {
+//   return (
+//     <Canvas dpr={[1, 1.5]} camera={{ fov: 35, position: [0, 5, 10] }}>
+//       <OrbitControls />
+//       <Stars />
+//       <ambientLight intensity={1.5} />
+//       <spotLight position={[5, 20, 15]} angle={5} />
+//       <mesh position={[0, 0, 0]}>
+//         <boxBufferGeometry attach="geometry" args={[5, 0.8, 5]} />
+//         <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
+//       </mesh>
+//     </Canvas>
+//   );
+// }
+
+// function PlayerScore() {
+//   return (
+//     <mesh position={[0, 1.5, -4]}>
+//       <Text
+//         scale={[5, 5, 5]}
+//         color={theme.NEON_GRE}
+//         anchorX="right"
+//         anchorY="middle"
+//       >
+//         {gamer1Name}
+//         {gamer1Score}
+//       </Text>
+//       <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
+//     </mesh>
+//   );
+// };
+
 function Basic() {
+  const gamer1Name = "Polabear : \n";
+  const gamer1Score = "0";
+
+  const gamer2Name = " Dall\n";
+  const gamer2Score = "999";
+
   return (
     <Canvas dpr={[1, 1.5]} camera={{ fov: 35, position: [0, 5, 10] }}>
       <OrbitControls />
       <Stars />
-      <ambientLight intensity={1.5} />
-      <spotLight position={[5, 20, 15]} angle={5} />
+      <ambientLight intensity={0} />
+      {/* <spotLight position={[5, 20, 15]} angle={5} /> */}
       <mesh position={[0, 0, 0]}>
-        <boxBufferGeometry attach="geometry" args={[5, 0.8, 5]} />
-        <meshLambertMaterial attach="material" color={theme.NEON_RED} />
+        <boxBufferGeometry attach="geometry" args={[5, 0.1, 5]} />
+        <meshLambertMaterial attach="material" color="#00FFF080" />
+      </mesh>
+      <mesh position={[0, 1.5, -4]}>
+        <Text
+          scale={[5, 5, 5]}
+          color={theme.NEON_GRE}
+          anchorX="right"
+          anchorY="middle"
+        >
+          {gamer1Name}
+          {gamer1Score}
+        </Text>
+        <Text
+          scale={[5, 5, 5]}
+          color={theme.NEON_GRE}
+          anchorX="left"
+          anchorY="middle"
+        >
+          {gamer2Name}
+          {gamer2Score}
+        </Text>
+        <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
       </mesh>
     </Canvas>
   );
@@ -50,3 +115,7 @@ export function ContainerContents() {
     </Contents>
   );
 }
+
+const Contents = styled(template.Contents, {
+
+});
