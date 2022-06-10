271bc1fe210bbe4e57c453aa8f58dce3af998c1e
KKWANH <kwanho0096@gmail.com>
2022-06-10 11:26:49 +0900

    🚧 #47 Profile 페이지 content 구성

--- src/container/contentProfile.tsx
+++ src/container/contentProfile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { styled } from "@stitches/react";
+import { Routes, Route, useParams } from "react-router-dom";
 import * as template from "./contentTemplate";
 import * as theme from "../theme/theme";
 
@@ -7,10 +8,13 @@ const Contents = styled(template.Contents, {
 
 });
 
+function ContentProfile() {
+  const { userId } = useParams();
+  return (<Contents>profile: {userId}</Contents>);
+}
+
 export function ContainerContents() {
   return (
-    <Contents className="contents">
-      Profile
-    </Contents>
+    <ContentProfile />
   );
 }
