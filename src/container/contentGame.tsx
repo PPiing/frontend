2905b5330da53f97f114d3a9a02b89ad5beafd45
hyunja27 <123gk@naver.com>
2022-06-19 16:29:49 +0900

    :construction: 06/19_construct_RacketMoving_fix_and_3D_name&score_success #30

--- src/container/contentGame.tsx
+++ src/container/contentGame.tsx
@@ -1,93 +1,87 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { styled, } from "@stitches/react";
-import * as THREE from "three";
 import { Canvas, extend } from "@react-three/fiber";
-import { CameraShake, OrbitControls, Stars, Text3D, Text } from "@react-three/drei";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+import { CameraShake, OrbitControls, Stars, Text, Text3D } from "@react-three/drei";
 import * as theme from "../theme/theme";
 import * as template from "./contentTemplate";
+import fontPath from "../font/Neon 80s_Regular.json";
 
-// => 기본 game Stage 렌더링중 _ 주석을 지우지 마세요!
-
-// const config = {
-//   maxYaw: 0.1, // Max amount camera can yaw in either direction
-//   maxPitch: 0.1, // Max amount camera can pitch in either direction
-//   maxRoll: 0.1, // Max amount camera can roll in either direction
-//   yawFrequency: 0.1, // Frequency of the the yaw rotation
-//   pitchFrequency: 0.1, // Frequency of the pitch rotation
-//   rollFrequency: 0.1, // Frequency of the roll rotation
-//   intensity: 0.8, // initial intensity of the shake
-//   decay: false, // should the intensity decay over time
-//   decayRate: 0.65, // if decay = true this is the rate at which intensity will reduce at
-//   controls: undefined, // if using orbit controls, pass a ref here so we can update the rotation
-// };
-// <CameraShake {...config} />
-// <Cloud opacity={0.5} speed={0.4} width={10} depth={1.5} segments={20} />
-
-// function Font() {
-//   return (
-//     <Text color="black" anchorX="center" anchorY="middle">
-//       hello world!
-//     </Text>
-//   );
-// }
-
-// function Basic2() {
-//   return (
-//     <Canvas dpr={[1, 1.5]} camera={{ fov: 35, position: [0, 5, 10] }}>
-//       <OrbitControls />
-//       <Stars />
-//       <ambientLight intensity={1.5} />
-//       <spotLight position={[5, 20, 15]} angle={5} />
-//       <mesh position={[0, 0, 0]}>
-//         <boxBufferGeometry attach="geometry" args={[5, 0.8, 5]} />
-//         <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
-//       </mesh>
-//     </Canvas>
-//   );
-// }
-
-// function PlayerScore() {
-//   return (
-//     <mesh position={[0, 1.5, -4]}>
-//       <Text
-//         scale={[5, 5, 5]}
-//         color={theme.NEON_GRE}
-//         anchorX="right"
-//         anchorY="middle"
-//       >
-//         {gamer1Name}
-//         {gamer1Score}
-//       </Text>
-//       <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
-//     </mesh>
-//   );
-// };
+extend({ TextGeometry });
 
 function Basic() {
   const [redRacketYPos, setRedRacketYPos] = useState(0);
-  const gamer1Name = "Polabear : \n";
+  const [blueRacketYPos, setBlueRacketYPos] = useState(0);
+  const gamer1Name = "Polabear\n";
   const gamer1Score = "0";
-  const gamer2Name = " Dall\n";
-  const gamer2Score = "999";
+  const gamer2Name = " Polabear\n";
+  const gamer2Score = "42";
+  const gameBoardHeight = 5;
+  const gameBoardWidth = 7;
 
-  let blueRacketYPos = 0;
+  const gameFont = new FontLoader().parse(fontPath);
 
-  const controlRacket = (e:any) => {
-    console.log("down => ", e.key, "  =>   ", redRacketYPos);
+  const RacketMoveSpeed = 0.6;
+  const RacketSize = 1;
+
+  const nameTextConfig = useMemo(
+    () => ({ size: 0.5,
+      height: 0.1,
+      curveSegments: 0.2,
+      bevelEnabled: true,
+      bevelThickness: 0.1,
+      bevelSize: 0.01,
+      bevelOffset: 0,
+      bevelSegments: 8 }),
+    []
+  );
+
+  const scoreTextConfig = useMemo(
+    () => ({ size: 2,
+      height: 0.1,
+      curveSegments: 0.2,
+      bevelEnabled: true,
+      bevelThickness: 0.1,
+      bevelSize: 0.01,
+      bevelOffset: 0,
+      bevelSegments: 8 }),
+    []
+  );
+
+  const controlMyRacket = (e:any) => {
+    // console.log(e.key, "  =>   ", redRacketYPos);
     if (e.key === "ArrowUp") {
-      setRedRacketYPos(redRacketYPos - 0.1);
+      if (redRacketYPos < (-gameBoardHeight / 2 + RacketSize * 0.6)) {
+        return -1;
+      }
+      setRedRacketYPos(redRacketYPos - RacketMoveSpeed);
     }
     if (e.key === "ArrowDown") {
-      setRedRacketYPos(redRacketYPos + 0.1);
+      if (redRacketYPos > (gameBoardHeight / 2 - RacketSize * 0.6)) {
+        return -1;
+      }
+      setRedRacketYPos(redRacketYPos + RacketMoveSpeed);
+    }
+    return 0;
+  };
+
+  const controlOppRacket = (e:any) => {
+    // console.log("down => ", e.key, "  =>   ", redRacketYPos);
+    if (e.key === "ArrowUp") {
+      setBlueRacketYPos(redRacketYPos - RacketMoveSpeed);
+    }
+    if (e.key === "ArrowDown") {
+      setBlueRacketYPos(redRacketYPos + RacketMoveSpeed);
     }
   };
 
   useEffect(() => {
-    window.addEventListener("keydown", controlRacket);
-  }, []);
-
-  // window.addEventListener("keydown", controlRacket);
-  // window.removeEventListener("keydown", controlRacket);
+    window.addEventListener("keydown", controlMyRacket);
+    return () => {
+      window.removeEventListener("keydown", controlMyRacket);
+    };
+  });
 
   return (
     <Canvas dpr={[1, 1.5]} camera={{ fov: 35, position: [0, 5, 10] }}>
@@ -97,7 +91,7 @@ function Basic() {
 
       {/* 메인 게임필드 */}
       <mesh position={[0, 0, 0]}>
-        <boxBufferGeometry attach="geometry" args={[7, 0.1, 5]} />
+        <boxBufferGeometry attach="geometry" args={[gameBoardWidth, 0.1, gameBoardHeight]} />
         <meshLambertMaterial attach="material" color="#000000" />
       </mesh>
 
@@ -120,25 +114,33 @@ function Basic() {
         <meshLambertMaterial attach="material" color="#FAFF00" />
       </mesh>
 
-      <mesh position={[0, 1.5, -4]}>
-        <Text
-          scale={[5, 5, 5]}
-          color={theme.NEON_GRE}
-          anchorX="right"
-          anchorY="middle"
-        >
-          {gamer1Name}
+      {/* Score & Name */}
+      <mesh position={[-3.5, 1, -4]}>
+        <Text3D font={fontPath} {...scoreTextConfig}>
           {gamer1Score}
-        </Text>
-        <Text
-          scale={[5, 5, 5]}
-          color={theme.NEON_GRE}
-          anchorX="left"
-          anchorY="middle"
-        >
-          {gamer2Name}
+          <meshNormalMaterial />
+        </Text3D>
+        <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
+      </mesh>
+      <mesh position={[-3.5, 0.2, -4]}>
+        <Text3D font={fontPath} {...nameTextConfig}>
+          {gamer1Name}
+          <meshNormalMaterial />
+        </Text3D>
+        <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
+      </mesh>
+      <mesh position={[0.7, 1, -4]}>
+        <Text3D font={fontPath} {...scoreTextConfig}>
           {gamer2Score}
-        </Text>
+          <meshNormalMaterial />
+        </Text3D>
+        <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
+      </mesh>
+      <mesh position={[0.5, 0.2, -4]}>
+        <Text3D font={fontPath} {...nameTextConfig}>
+          {gamer2Name}
+          <meshNormalMaterial />
+        </Text3D>
         <meshLambertMaterial attach="material" color={theme.NEON_BLU} />
       </mesh>
     </Canvas>
@@ -156,17 +158,6 @@ export function ContainerContents() {
 const Contents = styled(template.Contents, {
   "&:hover": {
     border: `3px solid ${theme.NEON_RED}`,
-    filter: "drop-shadow(0 0 2px gray)",
+    filter: "drop-shadow(0 0 2px gray)"
   },
 });
-
-// export const NeonHoverRed = styled("div", {
-//   border: "3px solid gray",
-//   transition: "all 0.5s",
-//   filter: "drop-shadow(0 0 2px gray)",
-//   "&:hover": {
-//     border: `3px solid ${NEON_RED}`,
-//     color: `${NEON_RED}`,
-//     filter: `drop-shadow(0 0 2px ${NEON_RED}) brightness(1.6)`,
-//   },
-// });
