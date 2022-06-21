c0e03612fb888ddce757941ceafd82fa402cade9
muing <ungetqqq@gmail.com>
2022-06-21 17:19:05 +0900

    :bug: #66 Add log for fix bug

--- src/component/rootControl.tsx
+++ src/component/rootControl.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import store from "../redux/store";
 import { ReducerType } from "../redux/rootReducer";
@@ -8,26 +8,29 @@ import { AuthData, setAuth } from "../redux/slices/auth";
 
 export function RootControl() {
   const auth = useSelector<ReducerType, AuthData>((state) => state.auth);
+  
+  const dispatch2 = useDispatch();
 
   // test
   console.log("in RootControl: ", auth);
 
+  let temp: boolean = false;
   // 요청 url 변경 필요.
   axios.get("https://bongcheonmountainclub.iptime.org/api/users/profile").then((response) => {
     if (response.status === 200) {
       // test
+      temp = true;
       console.log("in rootControl req: ", response);
-      useEffect(() => {
-        store.dispatch(setAuth({ auth: true } as AuthData));
-        // test
-        console.log("in rootControl req state: ", auth);
-      }, []);
+      store.dispatch(setAuth({ auth: true } as AuthData));
     }
   });
 
   if (!auth.auth || (auth.isRequire2f && !auth.auth2f)) {
-    return (<Navigate replace to="/login" />);
+    return (
+      <Navigate replace to="/login" />
+    );
   }
-
-  return (<Navigate replace to="/home" />);
+  return (
+    <Navigate replace to="/home" />
+  );
 }
