c6c8f733995a0d86eb65df4b744348f47e20ae77
muing <ungetqqq@gmail.com>
2022-06-21 21:37:56 +0900

    :hammer: #66 Redux persist apply for maintain state and fix redirect issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import App from "./App";
 import "./index.css";
 import store from "./redux/store";
 
+let persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import reducer from "./rootReducer";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export type AppDispatch = typeof store.dispatch
