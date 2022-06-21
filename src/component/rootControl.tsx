35f087bf6d4b70f47a03da042584ecb29d7745bf
muing <ungetqqq@gmail.com>
2022-06-21 17:22:06 +0900

    :bug: #66 Add log for fix bug

--- src/component/rootControl.tsx
+++ src/component/rootControl.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import store from "../redux/store";
 import { ReducerType } from "../redux/rootReducer";
@@ -8,18 +8,14 @@ import { AuthData, setAuth } from "../redux/slices/auth";
 
 export function RootControl() {
   const auth = useSelector<ReducerType, AuthData>((state) => state.auth);
-  
-  const dispatch2 = useDispatch();
 
   // test
   console.log("in RootControl: ", auth);
 
-  let temp: boolean = false;
   // 요청 url 변경 필요.
   axios.get("https://bongcheonmountainclub.iptime.org/api/users/profile").then((response) => {
     if (response.status === 200) {
-      // testt
-      temp = true;
+      // test
       console.log("in rootControl req: ", response);
       store.dispatch(setAuth({ auth: true } as AuthData));
     }
