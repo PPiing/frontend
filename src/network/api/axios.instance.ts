e8290fa32614dfc41d5eea61738c4b260dd59a75
HyeonsikBae <baehyeonsik@gmail.com>
2022-06-23 00:10:50 +0900

    feat: set up axios

---
+++ src/network/api/axios.instance.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import store from "../../redux/store";
+import { AuthData, setAuth } from "../../redux/slices/auth";
+
+export const instance = axios.create({
+  baseURL: `${window.location.origin}/api`,
+  timeout: 5000,
+});
+
+instance.interceptors.response.use((response) => {
+  store.dispatch(setAuth({ auth: true } as AuthData));
+  return response;
+}, (error) => {
+  store.dispatch(setAuth({ auth: false } as AuthData));
+  return Promise.reject(error);
+});
+
+export default instance;
