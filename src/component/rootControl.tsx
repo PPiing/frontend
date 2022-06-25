9dda29d8f831096267cd56228533cffd2332c790
muing <ungetqqq@gmail.com>
2022-06-25 19:28:18 +0900

    :bug: fixed mistake

--- src/component/rootControl.tsx
+++ src/component/rootControl.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { getLoggedUserProfile } from "../network/api/axios.custom";
 import { ReducerType } from "../redux/rootReducer";
 import { AuthData } from "../redux/slices/auth";
 
 export function RootControl() {
   const auth = useSelector<ReducerType, AuthData>((state) => state.auth);
+
+  getLoggedUserProfile();
 
   if (!auth.auth || (auth.isRequire2f && !auth.auth2f)) {
     return (<Navigate replace to="/login" />);
