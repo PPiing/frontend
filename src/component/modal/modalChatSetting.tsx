63f591f22a78ded998ed23ec49664da61221fbf2
KKWANH <kwanho0096@gmail.com>
2022-08-07 17:14:37 +0900

    :sparkles: input form

--- src/component/modal/modalChatSetting.tsx
+++ src/component/modal/modalChatSetting.tsx
@@ -26,21 +26,29 @@ const SettingTitle = styled("div", {
 const SettingH1 = styled("div", {
   fontSize: "2.3vh",
   fontWeight: "300",
+  textShadow: "0px 0px 2px #ffffff",
+});
+
+const SettingInputText = styled("input", {
+  width: "calc(98% - 10px)",
+  height: "2vh",
+  marginLeft: "1%",
+  marginTop: "0.5vh",
+  padding: "5px",
+
+  background: "none",
+  border: "0.5px solid white",
+
+  fontSize: "1.5vh",
+  color: "white",
   textShadow: "0px 0px 1px #ffffff",
 });
 
 function RoomName(props: any) {
+  const { chatInfo } = props;
   return (
     <div>
-      <input type="text" />
-    </div>
-  )
-}
-
-function RoomPassword(props: any) {
-  return (
-    <div>
-      <input type="text" />
+      <SettingInputText type="text" defaultValue={chatInfo.chatName} />
     </div>
   )
 }
@@ -53,10 +61,26 @@ function RoomPublic(props: any) {
   )
 }
 
+function RoomPassword(props: any) {
+  return (
+    <div>
+      <SettingInputText type="password" placeholder="Don't " />
+    </div>
+  )
+}
+
+function RoomChangeButton(props: any) {
+  return (
+    <div>
+      <button type="button">수정하기</button>
+    </div>
+  )
+}
+
 function RoomInvite(props: any) {
   return (
     <div>
-      <input type="text" />
+      <SettingInputText type="text" />
     </div>
   )
 }
@@ -65,20 +89,26 @@ export function ModalChatSetting(props: any) {
   const { chatInfo } = props;
   const loggedUser = useSelector<ReducerType, LoggedUserData>((state) => state.loggedUser);
 
+  console.log("낌타임!!");
+  console.log(chatInfo);
+  console.log(loggedUser);
+
   return (
     <SettingZone>
       <SettingTitle>Setting</SettingTitle>
       <SettingH1>Room Name</SettingH1>
-      <RoomName />
-      <br />
-      <SettingH1>Room Password</SettingH1>
-      <RoomPassword />
+      <RoomName chatInfo={chatInfo} />
       <br />
       <SettingH1>Room Public</SettingH1>
-      <RoomPublic />
+      <RoomPublic chatInfo={chatInfo} />
+      <br />
+      <SettingH1>Room Password</SettingH1>
+      <RoomPassword chatInfo={chatInfo} />
+      <br />
+      <RoomChangeButton />
       <br />
       <SettingH1>Room Invite</SettingH1>
-      <RoomInvite />
+      <RoomInvite chatInfo={chatInfo} />
       <br />
 
     </SettingZone>
