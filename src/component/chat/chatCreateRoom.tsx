9af83dafc51d18847ef23b2c8dc6036b4f59508e
KKWANH <kwanho0096@gmail.com>
2022-08-12 21:16:24 +0900

    :sparkles: chat create room

--- src/component/chat/chatCreateRoom.tsx
+++ src/component/chat/chatCreateRoom.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { styled } from "@stitches/react";
 import axios from "axios";
+import "./chatCreateRoom.css"
 
 enum ChatType {
   CHTP10 = "CHTP10", // 개인 채팅방 (DM)
@@ -9,49 +10,63 @@ enum ChatType {
   CHTP40 = "CHTP40", // 비밀 채팅방 (private)
 }
 
-const ChatCreateRoom = styled("div", {
+const Wrapper = styled("div", {
+  width: "100%",
+  height: "100%",
+});
+
+const CreateZone = styled("div", {
+  color: "white",
+  width: "90%",
+  marginLeft: "5%",
+  height: "100%",
+  overflowX: "hidden",
+  overflowY: "hidden",
+});
+
+const TitleZone = styled("div", {
   display: "flex",
+  alignItems: "center",
   flexDirection: "column",
   justifyContent: "center",
-  fontSize: "3rem",
-  borderRadius: "5%",
-  width: "95%",
-  height: "95%",
+  textAlign: "center",
+  height: "30%",
 });
 
-const InputArea = styled("div", {
-  display: "flex",
-  flexDirection: "row",
-  position: "relative",
-  left: "3rem",
-  margin: "1rem",
+const Title = styled("div", {
+  fontSize: "5vh",
+  fontWeight: "bold",
+  textShadow: "0px 0px 2px #ffffff",
+  marginTop: "4vh",
+  marginBottom: "-2vh",
 });
 
-const TextInput = styled("input", {
-  fontSize: "3rem",
-  width: "400px",
-  height: "50px",
-  position: "absolute",
-  left: "25rem",
+const H1 = styled("div", {
+  fontSize: "27px",
+  fontWeight: "600",
+  textShadow: "0px 0px 2px #ffffff",
+  marginBottom: "-1vh",
 });
 
-const RadioInput = styled("div", {
-  display: "flex",
-  flexDirection: "row",
+const InputZone = styled("div", {
+  height: "15%",
+  marginBottom: "2%",
 });
 
-const Text = styled("div", {
-  fontSize: "2rem",
-  position: "relative",
-  top: "1rem",
-  left: "8.5rem",
-});
+const InputText = styled("input", {
+  width: "calc(98% - 10px)",
+  height: "3vh",
+  marginLeft: "1%",
+  marginTop: "0.5vh",
+  padding: "5px",
+  borderRadius: "5px",
 
-const Radio = styled("input", {
-  position: "relative",
-  top: "2rem",
-  left: "8rem",
-  marginLeft: "3rem",
+  background: "none",
+  border: "0.5px solid white",
+
+  fontSize: "2vh",
+  color: "white",
+  textShadow: "0px 0px 1px #ffffff",
 });
 
 const ButtonArea = styled("div", {
@@ -59,19 +74,30 @@ const ButtonArea = styled("div", {
   flexDirection: "row",
   justifyContent: "space-around",
   margin: "3rem",
+  height: "20%",
 });
 
 const NeonBox = styled("div", {
-  width: "20rem",
-  height: "3rem",
-  borderRadius: "10px",
-  backgroundColor: "transparent",
-  fontWeight: "bold",
-  fontSize: "2.3rem",
+  width: "20vw",
+  height: "8vh",
+
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+
+  fontWeight: "bold",
+  fontSize: "2.3rem",
+
+  transition: "all 0.5s",
+  borderRadius: "10px",
+  background: "none",
   color: "grey",
+  cursor: "pointer",
+
+  "&:hover": {
+    color: "white",
+    background: "rgba(255, 255, 255, 0.2)",
+  },
 });
 
 export function CreateRoom(props: any) {
@@ -109,54 +135,98 @@ export function CreateRoom(props: any) {
   방 생성 성공 시 우측 컨텐츠 화면 empty 상태로 변경
   */
   const createRoom = () => {
-    axios.post("/api/chatrooms/new", {
+    // console.log(roomName, roomPassword, roomType);
+    axios.post("/api/chatrooms/new/", {
       chatType: roomType,
       chatName: roomName,
       password: roomPassword,
       isDirected: false,
-    })
-      .then((response) => {
-        console.log(response);
-        propFunc("empty");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    }).then((response) => {
+      console.log(response);
+      propFunc("empty");
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
   return (
-    <ChatCreateRoom>
-      <InputArea>
-        RoomName
-        <TextInput value={roomName} onChange={(event) => nameChange(event)} />
-      </InputArea>
-      <InputArea>
-        RoomType
-        <RadioInput>
-          <Radio type="radio" checked={roomType === ChatType.CHTP20} onChange={() => setType(ChatType.CHTP20)} />
-          <Text>PUBLIC</Text>
-        </RadioInput>
-        <RadioInput>
-          <Radio type="radio" checked={roomType === ChatType.CHTP40} onChange={() => setType(ChatType.CHTP40)} />
-          <Text>PRIVATE</Text>
-        </RadioInput>
-        <RadioInput>
-          <Radio type="radio" checked={roomType === ChatType.CHTP30} onChange={() => setType(ChatType.CHTP30)} />
-          <Text>PROTECTED</Text>
-        </RadioInput>
-      </InputArea>
-      <InputArea>
-        RoomPassword
-        <TextInput
-          value={roomPassword}
-          disabled={roomType !== ChatType.CHTP30}
-          onChange={(event) => passwordChange(event)}
-        />
-      </InputArea>
-      <ButtonArea>
-        <NeonBox onClick={() => createRoom()}>Create</NeonBox>
-        <NeonBox onClick={() => propFunc("empty")}>Close</NeonBox>
-      </ButtonArea>
-    </ChatCreateRoom>
+    <Wrapper>
+      <CreateZone>
+        <TitleZone>
+          <Title>Create Chat Room</Title>
+          <br />
+          <pre style={{ fontSize: "2.3vh", }}>
+            Create a new chat room.<br />
+            You will be the owner.
+          </pre>
+        </TitleZone>
+
+        <InputZone>
+          <H1>Room Name</H1>
+          <pre style={{ fontSize: "15px", marginBottom: "1.5vh", }}>
+            Create a new chat room. You will be the owner.
+          </pre>
+          <InputText value={roomName} onChange={(event) => nameChange(event)} />
+        </InputZone>
+
+        <InputZone>
+          <H1>Room Type</H1>
+          <pre style={{ fontSize: "15px", marginBottom: "1.5vh", }}>
+            <b>public: </b>&nbsp;&nbsp;&nbsp;No password, anyone can join freely<br />
+            <b>protected: </b>Password exists, anyone can join after entering<br />
+            <b>private: </b>&nbsp;&nbsp;Must be invited to enter
+          </pre>
+          <div className="radioWrapper">
+            <input
+              type="Radio"
+              name="select"
+              id="option-1"
+              checked={roomType === ChatType.CHTP20}
+              onChange={() => setType(ChatType.CHTP20)}
+            />
+            <input
+              type="Radio"
+              name="select"
+              id="option-2"
+              checked={roomType === ChatType.CHTP40}
+              onChange={() => setType(ChatType.CHTP40)}
+            />
+            <input
+              type="Radio"
+              name="select"
+              id="option-3"
+              checked={roomType === ChatType.CHTP30}
+              onChange={() => setType(ChatType.CHTP30)}
+            />
+            <label htmlFor="option-1" className="option option-1">
+              <span>public</span>
+            </label>
+            <label htmlFor="option-2" className="option option-2">
+              <span>private</span>
+            </label>
+            <label htmlFor="option-3" className="option option-3">
+              <span>protected</span>
+            </label>
+          </div>
+        </InputZone>
+
+        <InputZone>
+          <H1>Room Password</H1>
+          <pre style={{ fontSize: "15px", marginBottom: "1.5vh", }}>
+            <b>public: </b>No password, anyone can join freely
+          </pre>
+          <InputText
+            value={roomPassword}
+            disabled={roomType !== ChatType.CHTP30}
+            onChange={(event) => passwordChange(event)}
+          />
+        </InputZone>
+
+        <ButtonArea>
+          <NeonBox onClick={() => createRoom()}>Create</NeonBox>
+          <NeonBox onClick={() => propFunc("empty")}>Close</NeonBox>
+        </ButtonArea>
+      </CreateZone>
+    </Wrapper>
   );
 }
